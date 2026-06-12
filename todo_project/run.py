c602aa682bf90c3cbf1127bc7da56f48a4e27f97
andreyangelo <andrey_ams@hotmail.com>
2026-06-12 00:58:06 -0300

    Fix Bandit security findings

--- todo_project/run.py
+++ todo_project/run.py
@@ -1,4 +1,9 @@
 from todo_project import app
+import os
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8080)
+    app.run(
+        debug=os.getenv("FLASK_DEBUG", "False") == "True",
+        host=os.getenv("FLASK_HOST", "127.0.0.1"),
+        port=int(os.getenv("FLASK_PORT", "8080"))
+    )
--- todo_project/todo_project/__init__.py
+++ todo_project/todo_project/__init__.py
@@ -2,18 +2,14 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
+import os
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '45cf93c4d41348cd9980674ade9a7356'
+
+app.config['SECRET_KEY'] = os.getenv(
+    'SECRET_KEY',
+    'dev-secret-key'
+)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-db = SQLAlchemy(app)
-
-login_manager = LoginManager(app)
-login_manager.login_view = 'login' 
-login_manager.login_message_category = 'danger'
-
-bcrypt = Bcrypt(app)
-
-# Always put Routes at end
-from . import routes
